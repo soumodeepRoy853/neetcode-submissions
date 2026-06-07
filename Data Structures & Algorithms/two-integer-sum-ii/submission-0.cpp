8db4d63021e338df972f099738ee885cc622695a
Soumodeep Roy <83583312+soumodeepRoy853@users.noreply.github.com>
2026-06-07 22:12:48 +0530

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        //Using Two pointers
+        int n = numbers.size();
+        int left = 0, right = n-1;
+
+        while(left < right){
+            int sum = numbers[left] + numbers[right];
+
+            if(sum == target){
+                return{ left + 1, right + 1};
+            } else if(sum > target){
+                right --;
+            } else{
+                left ++;
+            }
+        }
+    }
+};
